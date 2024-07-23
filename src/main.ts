dad090108a0f3c294c3e73a86427dfb7f7f41751
sunny-sk <sk956223@gmail.com>
2024-07-23 12:07:41 +0530

    Project cleaned

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { MessagesModule } from './messages/messages.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create();
   await app.listen(3000);
 }
 bootstrap();
